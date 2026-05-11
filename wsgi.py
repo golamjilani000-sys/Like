5af05ae5a6d2e3d91521c4da199c455bbd036103
golamjilani000-sys <golamjilani000@gmail.com>
2026-05-11 20:15:22 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,5 @@
 from app import app
 
-# Vercel requires the application object to be exposed as 'app'
-# Do NOT include app.run() here as it causes the 'FUNCTION_INVOCATION_FAILED' crash.
-app = app
-
+if __name__ == '__main__':
+    app.run(debug=True)
+  
